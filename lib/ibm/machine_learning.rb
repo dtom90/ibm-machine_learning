5bdadc05f4af08e60d340f9055ca5d32106a1247
David Thomason <dlthomas@us.ibm.com>
2017-05-10 19:48:16 -0400

    Working z/OS token fetch

--- lib/ibm/machine_learning.rb
+++ lib/ibm/machine_learning.rb
@@ -18,6 +18,13 @@ module IBM
         'https://ibm-watson-ml.mybluemix.net/v2/identity/token'
       end
 
+      def ldap_request(http, url)
+        http.use_ssl = true
+        request      = Net::HTTP::Get.new url
+        request.basic_auth @username, @password
+        request
+      end
+
     end
 
     class Zos
@@ -37,18 +44,22 @@ module IBM
         "http://#{@host}:#{@ldap_port}/v2/identity/ldap"
       end
 
+      def ldap_request(http, url)
+        request = Net::HTTP::Post.new(url)
+        request.set_content_type 'application/json'
+        request.body = { username: @username, password: @password }.to_json
+        request
+      end
+
     end
 
     def fetch_token
       url = URI ldap_url
 
-      http         = Net::HTTP.new url.host, url.port
-      http.use_ssl = true
+      http = Net::HTTP.new url.host, url.port
 
-      request = Net::HTTP::Get.new url
-      request.basic_auth @username, @password
-
-      response = http.request request
+      ldap_request(http, url)
+      response = http.request ldap_request(http, url)
       raise response.class.to_s if response.is_a? Net::HTTPClientError
 
       JSON.parse(response.read_body)['token']
--- spec/ibm/machine_learning_spec.rb
+++ spec/ibm/machine_learning_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe IBM::MachineLearning do
     expect(IBM::MachineLearning::VERSION).not_to be nil
   end
 
-  it 'gets a token' do
+  it 'gets a token from Watson Machine Learning' do
     service = IBM::MachineLearning::Watson.new ENV['USERNAME'], ENV['PASSWORD']
+    token = service.fetch_token
+    expect(token).to be_a(String)
+  end
+
+  it 'gets a token from Machine Learning for z/OS' do
+    service = IBM::MachineLearning::Zos.new ENV['MLZ_USERNAME'], ENV['MLZ_PASSWORD'], ENV['MLZ_HOST'], ENV['MLZ_LDAP_PORT'], nil
     token = service.fetch_token
     expect(token).to be_a(String)
   end
